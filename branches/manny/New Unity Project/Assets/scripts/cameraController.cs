7cd6850d72272af964352b68b5266b97bc565413
captaincactuz <emanmomot@gmail.com>
2014-03-18 10:08:27 -0400

    add branches

---
+++ branches/manny/New Unity Project/Assets/scripts/cameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraController : MonoBehaviour {
+
+	// Use this for initialization
+	public Camera cam;
+	public player player;
+	void Start () {
+		cam = GetComponent ("Camera") as Camera;
+
+	}
+
+	// Update is called once per frame
+	public void Update() {
+		cam.transform.position = player.transform.position - player.transform.TransformDirection (new Vector3(0,-4,8));
+		cam.transform.LookAt (player.transform.position);
+	}
+}
---
+++ branches/manny/New Unity Project/Assets/scripts/player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public static player instance;
+
+	public Rigidbody body;
+	public Quaternion rotation = Quaternion.identity;
+
+
+	void Start () {
+
+		body = GetComponent ("Rigidbody") as Rigidbody;
+		body.freezeRotation = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float axis = Input.GetAxis ("Horizontal");
+		var rot = transform.rotation.eulerAngles;
+		float desRot = -80 * axis;
+		float rotAmt = dif (desRot,rot.z)/10;
+		rotation *= Quaternion.Euler (new Vector3 (0, 0, rotAmt));
+		transform.rotation = Quaternion.identity;
+		
+		//if (Input.GetAxis ("Jump") == 1) {
+			Vector3 f =  Quaternion.AngleAxis(transform.eulerAngles.z,new Vector3(0,1,0)) * transform.forward;
+			f.Normalize ();
+			transform.rotation = Quaternion.Euler(new Vector3(0,axis*180,0)) * rotation;
+			//transform.rotation *= Quaternion.AngleAxis(-rotAmt,new Vector3(0,1,0));
+			body.AddForce (10 * transform.forward);
+			Debug.Log (f);
+		//}
+		
+		//transform.rotation *= rotation;
+		
+			
+	}
+
+	float dif(float rot1, float rot2){
+		float val = rot1 - rot2;
+		if (Mathf.Abs (val) > 180)
+			return - Mathf.Sign (val) * (360 - Mathf.Abs (val));
+
+		return (rot1 - rot2);
+	}
+
+}
